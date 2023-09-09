16b4d64cd4a3bc828a79b8f231189cc7f48aa447
Andrew Ma <98911103+BobJr23@users.noreply.github.com>
2023-09-09 08:35:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+
+def get_message(channel, key):
+    message = list(
+        reversed(
+            requests.get(
+                f"https://discord.com/api/v9/channels/{channel}/messages",
+                headers={"authorization": key},
+            ).json()
+        )
+    )
+    return message
+
+
+def get_dms(key):
+    dms = requests.get(
+        "https://discord.com/api/users/@me/channels",
+        headers={"authorization": key},
+    ).json()
+    return dms
+
+
+def get_guilds(key):
+    guilds = requests.get(
+        "https://discord.com/api/users/@me/guilds",
+        headers={"authorization": key},
+    ).json()
+    return guilds
+
+
+def get_channels(server, key):
+    channels = requests.get(
+        f"https://discord.com/api/guilds/{server}/channels",
+        headers={"authorization": key},
+    ).json()
+    return channels
+
+
+def send_message(channel, key, content):
+    requests.post(
+        f"https://discord.com/api/v9/channels/{channel}/messages",
+        data={"content": content},
+        headers={"authorization": key},
+    )
+
+
+@app.route(r"/")
+def home():
+    return render_template(
+        "index.html",
+    )
+
+
+@app.route(r"/<guild>/<int:server>/", methods=["POST", "GET"])
+def channels(server, guild):
+    if "send" in request.form.keys():
+        key, message = request.form["token"], request.form["message"]
+        send_message(server, key, message)
+        guilds, message = get_guilds(key), get_message(server, key)
+    else:
+        key = request.form["token"]
+        message = False
+        if guild == "guild":
+            guilds = get_guilds(key)
+        elif guild == "channel":
+            guilds = get_channels(server, key)
+        elif guild == "dm":
+            guilds = get_dms(key)
+        else:
+            guilds = get_dms(key)
+            message = get_message(server, key)
+
+    return render_template(
+        "index.html", key=key, guild_check=guild, messages=message, guilds=guilds
+    )
+
+
+# if __name__ == "__main__":
+#     app.run(
+#         debug=True,
+#     )
