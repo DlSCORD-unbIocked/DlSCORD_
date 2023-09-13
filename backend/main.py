02328a782218262a0c3502c0fcfc473590a0e2e0
Divkov575 <dmitriy@ivkov.net>
2023-09-12 23:22:12 -0400

    began logic conversion react js

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+from flask.logging import default_handler
+import logging
+import json
+
 import os
-from backend.lib import send_message, get_guilds, get_message, get_channels, get_dms
+from lib import send_message, get_guilds, get_message, get_channels, get_dms
 
-app = Flask(__name__, static_folder="./static")
-
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
+# frontend_path = "../frontend/build"
 
 @app.route("/")
+@app.route("/messages")
 def about():
     """
     returns frontend
     Note: only 1 path ("/") is necessary because react js handles all other paths
     """
-    return render_template(os.path.join("../frontend/build/index.html"))
+    # return render_template("index.html")
+    return send_file("../frontend/build/index.html")
 
 
 @app.route(r"/<guild>/<int:server>/", methods=["POST", "GET"])
 def channels(server, guild):
+    content = json.loads(request.data)
+
     if "send" in request.form.keys():
         key, message = request.form["token"], request.form["message"]
         send_message(server, key, message)
@@ -31,14 +39,16 @@ def channels(server, guild):
         else:
             snav = get_dms(key)
             message = get_message(server, key)
-    return render_template(
-        "index.html",
-        key=key,
-        guild_check=guild,
-        messages=message,
-        guilds=guilds,
-        snav=snav,
-    )
+
+    return "Success", 201
+#     return render_template(
+#         "index.html",
+#         key=key,
+#         guild_check=guild,
+#         messages=message,
+#         guilds=guilds,
+#         snav=snav,
+#     )
 
 
 if __name__ == "__main__":
--- frontend/src/login/login.jsx
+++ frontend/src/login/login.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react'
 import Cookies from 'universal-cookie';
 import './login.scss';
+import {Link} from "react-router-dom";
+
 function Login() {
     const [token, setToken] = useState("");
+    const handleTokenChange = (event) => {
+        setToken(event.target.value);
+    };
   const login = async (e) => {
-      // e.preventDefault()
-      // Cookies.set("token", token)
-      // console.log("logging in")
+      e.preventDefault()
+      document.cookie = "token="+token
       window.open("messages", "_self");
-
   };
 
   return (
       <div>
-          <h1>lolol</h1>
-            <input onChange={setToken}  value={token} id="tokenPaste" type="text" placeholder="put token heree" />
+          <h1>aklsdfl;asjdflol</h1>
+            <input onChange={handleTokenChange}  value={token.value} id="tokenPaste" type="text" placeholder="put token heree" />
             <button type="submit" id="submitToken" onClick={login}>submit</button>
+            {/*<Link to={"/messages"} onClick={login()}>Submit</Link>*/}
       </div>
   );
 }
 
 export default Login;
+// "NTg2MTU1ODUzMTU4Mjg1MzIx.GMXELS.faHuJ74NJzCXmDTS9lXcOJsXD4yu4mj39lyHrw"
